d1398be7a2ef0205005be5375b4f9992d9ce70f5
unknown <eugenie.porre@etu.minesparis.psl.eu>
2025-01-31 14:54:21 +0100

    résoluation de quelques erreurs

--- superrogue/Monster.py
+++ superrogue/Monster.py
@@ -5,33 +5,30 @@ def norme(positon1,position2):
     return (positon1[0]-position2[0])**2 + (positon1[1]-position2[1])**2
 
 class Monster:
-   """ Class to define a monster"""
-    def __init__(self, position, degats, vies,name) -> None:
-            """Object initialization."""
+    def __init__(self,position,damages,lives,name) -> None:
+            """Object initialisation"""
             # Vérification que 'position' est un tableau numpy avec 2 dimensions
             if not isinstance(position, np.ndarray):
                 raise TypeError("Position du monstre doit être un tableau NumPy.")
             if position.shape != (2,):
                 raise ValueError("Position du monstre doit être un vecteur à 2 dimensions.")
             self._position = position
-            self._degats = degats
-            self._vies = vies
+            self._damages = damages
+            self._lives = lives
             self._name = name
 
-    def monster_move(self._position,hero,mur):
-
+    def monster_move(self,hero,wall:list):
         if norme(self._position,hero._position) <= 1:
-            hero._vies = 
+            hero._lives = 
         else :
             move = np.array([0,1],[0,-1],[1,0],[-1,0])
             distance =[]
             for m in move:
                 next_monster_position = self._position + move
-                if next_monster_position in mur:
+                if next_monster_position in wall:
                     distance.append(float('inf')
                 else : 
                     distance.append(norme(next_monstre_position,hero._position))
                 
-            self._position += move[liste.index(min(distance))]
+            self._position += move[distance.index(min(distance))]
         return
-    
