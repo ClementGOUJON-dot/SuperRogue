62b7bbcff55dbe67fba9a11817b70036f8cdade1
Eleonore Leroy <ele.leroy05@gmail.com>
2025-01-31 11:40:36 +0100

    ajout code eleonore

---
+++ main.py
@@ -0,0 +1,16 @@
+from .lignecommande import lignecommande
+from .game import Game 
+
+def main() -> None :
+    try :
+        args = lignecommande()
+
+        Game(width = args.width, 
+             height = args.height, 
+             tile_size = args.tile_size, 
+             fps = args.fps, 
+             wall_color = args.wall_color, 
+             door_color = args.door_color, 
+             heroe_color = args.heroe_color)
+        
+        
