34a8a5644f617d76c783200a94cf81a02dcc6d55
Clément Goujon <clement.goujon@etu.minesparis.psl.eu>
2025-01-31 14:58:12 +0100

    coucou

--- superrogue/Monster.py
+++ superrogue/Monster.py
@@ -1,12 +1,12 @@
 import pygame
 import numpy as np
+import
 
 def norme(positon1,position2):
     return (positon1[0]-position2[0])**2 + (positon1[1]-position2[1])**2
 
-class Monster:
    """ Class to define a monster"""
-    def __init__(self, position, degats, vies,name) -> None:
+    def __init__(self, position, damages, lives,name) -> None:
             """Object initialization."""
             # Vérification que 'position' est un tableau numpy avec 2 dimensions
             if not isinstance(position, np.ndarray):
@@ -14,14 +14,18 @@ class Monster:
             if position.shape != (2,):
                 raise ValueError("Position du monstre doit être un vecteur à 2 dimensions.")
             self._position = position
-            self._degats = degats
-            self._vies = vies
+            self._damages = damages
+            self._lives = lives
             self._name = name
 
     def monster_move(self._position,hero,mur):
 
         if norme(self._position,hero._position) <= 1:
-            hero._vies = 
+            hero.lives += -self._damages
+            if hero._lives > 0 :
+                print(self._name, " hurt you... :( ")
+                print("You lost ",self._degats, " lives")
+
         else :
             move = np.array([0,1],[0,-1],[1,0],[-1,0])
             distance =[]
@@ -34,4 +38,7 @@ class Monster:
                 
             self._position += move[liste.index(min(distance))]
         return
+    
+    
+
     
